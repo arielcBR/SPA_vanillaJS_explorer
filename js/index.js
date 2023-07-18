88dd7ea455ef24719ff7da528091ccb4c85f4e77
Ariel Campos <ariel.campos94@yahoo.com>
2023-07-18 00:27:21 -0300

    encapsulando com classe

--- js/index.js
+++ js/index.js
@@ -1,37 +1,12 @@
-const routes = {
-    "/": "/pages/home.html",
-    "/universe": "/pages/universe.html",
-    "/explorer": "/pages/explorer.html",
-    404: "/pages/404.html",
-}
+import { Router } from './router.js'
 
-function route(event) {
-    event.preventDefault()
-    window.history.pushState({}, "", event.target.href)
+const router = new Router()
+router.add("/", "/pages/home.html")
+router.add("/universe", "/pages/universe.html")
+router.add("/explorer", "/pages/explorer.html")
+router.add(404, "/pages/404.html")
 
-    handle()
+router.handle()
 
-}
-
-function handle() {
-    const { pathname } = window.location
-    const route = routes[pathname] || routes[404]
-
-    fetch(route)
-        .then(data => data.text())
-        .then(html => {
-            const styleSheet = document.querySelector("#css")
-
-            if (pathname == "/explorer") {
-                styleSheet.href = "./css/explorer.css"
-            }
-            else if (pathname == "/universe") {
-                styleSheet.href = "./css/universe.css"
-            }
-            else {
-                styleSheet.href = "./css/home.css"
-            }
-            
-            document.querySelector('#app').innerHTML = html
-        })
-}
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
--- js/router.js
+++ js/router.js
@@ -1,4 +1,4 @@
-class Router {
+export class Router {
     routes = {}
 
     add(routeName, page) {
@@ -6,9 +6,7 @@ class Router {
     }
 
     route(event) {
-        event = event || window.event
         event.preventDefault()
-
         window.history.pushState({}, "", event.target.href)
         this.handle()
     }
@@ -20,11 +18,19 @@ class Router {
         fetch(route)
             .then(data => data.text())
             .then(html => {
+                const styleSheet = document.querySelector("#css")
+
+                if (pathname == "/explorer") {
+                    styleSheet.href = "./css/explorer.css"
+                }
+                else if (pathname == "/universe") {
+                    styleSheet.href = "./css/universe.css"
+                }
+                else {
+                    styleSheet.href = "./css/home.css"
+                }
+
                 document.querySelector('#app').innerHTML = html
             })
-
     }
-
 }
-
-export default new Router()
